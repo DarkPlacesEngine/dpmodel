fcff5140858a9e7b42977a89109b44e83935b36f
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2006-02-05 20:29:04 +0000

    added example files
    git-svn-id: svn://svn.icculus.org/twilight/trunk/dpmodel@5946 d7cf8633-e32d-0410-b094-e92efae38249

---
+++ box.h
@@ -0,0 +1,14 @@
+/*
+Generated header file for box
+This file contains frame number definitions for use in code referencing the model, to make code more readable and maintainable.
+*/
+
+#ifndef MODEL_BOX_H
+#define MODEL_BOX_H
+
+#define MODEL_BOX_BOX_0 0
+#define MODEL_BOX_BOX_START 0
+#define MODEL_BOX_BOX_END 1
+#define MODEL_BOX_BOX_LENGTH 1
+
+#endif /*MODEL_BOX_H*/
